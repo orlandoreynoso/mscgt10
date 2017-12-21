a7a098374f8d31f8a330ad4d59382d152c0ecf6a
orlandoreynoso <orlandoreynoso01@gmail.com>
2017-12-21 12:13:44 -0600

    update banner

--- template/cover-slide.php
+++ template/cover-slide.php
@@ -2,8 +2,8 @@
     <div class="row">
       <div class="col-md-12">
         <div>
-        	<?php // web  echo do_shortcode("[metaslider id=4323]");  ?>
-        	<?php  echo do_shortcode("[metaslider id=49]");  ?>        	
+        	<?php echo do_shortcode("[metaslider id=4323]");  ?>
+        	<?php // echo do_shortcode("[metaslider id=49]");  ?>
         </div>
       </div>
     </div>
